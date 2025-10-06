65753bf65ef94f40854f39470f6845707ef49238
yingdth <vinhufworks@gmail.com>
2025-10-06 20:53:15 +0700

    Okay?

---
+++ backend/discovery-service/src/main/java/com/ecommerce/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ backend/user-service/src/main/java/com/ecommerce/user/entity/User.java
@@ -0,0 +1,4 @@
+package com.ecommerce.user.entity;
+
+public class User {
+}
